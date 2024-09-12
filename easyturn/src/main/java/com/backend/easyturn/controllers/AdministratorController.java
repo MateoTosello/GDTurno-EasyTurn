adf2e84d61d966903ef03b029903e073943c11c5
BochatayMauricio <bochataymauricio01@gmail.com>
2024-09-12 11:49:43 -0300

    CRUD ADMIN (falta terminar)

--- easyturn/src/main/java/com/backend/easyturn/controllers/AdministratorController.java
+++ easyturn/src/main/java/com/backend/easyturn/controllers/AdministratorController.java
@@ -1,8 +1,11 @@
 package com.backend.easyturn.controllers;
 
 import com.backend.easyturn.entities.Administrator;
+import com.backend.easyturn.entities.DTOs.AdministratorDTO;
 import com.backend.easyturn.services.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,30 +18,38 @@ public class AdministratorController {
 
     @PostMapping(path="/post")
     @ResponseBody
-    public int createAdministrator(@RequestBody Administrator administrator) {
-        return this.administratorService.createAdministrator(administrator);
+    public ResponseEntity<AdministratorDTO> createAdministrator(@RequestBody Administrator administrator) {
+        AdministratorDTO administratorDTO = this.administratorService.createAdministrator(administrator);
+        return new ResponseEntity<>(administratorDTO, HttpStatus.CREATED);
     }
 
-    @GetMapping(path="/getAdministrators")
+    @GetMapping(path="/get-administrators")
     @ResponseBody
-    public List<Administrator> getAdministrators(){
-        return this.administratorService.getAllAdministrator();
+    public ResponseEntity<List<AdministratorDTO>> getAdministrators(){
+        List<AdministratorDTO> administrators = this.administratorService.getAllAdministrator();
+        if(administrators.isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(administrators, HttpStatus.OK);
     }
 
     @DeleteMapping(path="/delete/{id}")
-    public void deleteAdministrator(@PathVariable int id) {
+    public ResponseEntity deleteAdministrator(@PathVariable int id) {
         this.administratorService.deleteAdministrator(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(path = "/getAdministrator/{idAdmin}")
+    @GetMapping(path = "/get-administrator/{idAdmin}")
     @ResponseBody
-    public Administrator getAdministrator(@PathVariable int idAdmin){
-        return this.administratorService.getAdministrator(idAdmin);
+    public ResponseEntity<AdministratorDTO> getAdministrator(@PathVariable int idAdmin){
+        AdministratorDTO administrator = this.administratorService.getAdministrator(idAdmin);
+        return new ResponseEntity<>(administrator, HttpStatus.OK);
     }
-
+    
     @PutMapping("/update")
     @ResponseBody
-    public Administrator updateAdministrator(@RequestBody Administrator administrator) {
-        return this.administratorService.updateAdministrator(administrator);
+    public ResponseEntity<AdministratorDTO> updateAdministrator(@RequestBody Administrator administrator) {
+        AdministratorDTO administratorDTO = this.administratorService.updateAdministrator(administrator);
+        return new ResponseEntity<>(administratorDTO, HttpStatus.OK);
     }
 }
--- easyturn/src/main/java/com/backend/easyturn/services/AdministratorService.java
+++ easyturn/src/main/java/com/backend/easyturn/services/AdministratorService.java
@@ -1,7 +1,7 @@
 package com.backend.easyturn.services;
 
 import com.backend.easyturn.entities.Administrator;
-import com.backend.easyturn.entities.Appointment;
+import com.backend.easyturn.entities.DTOs.AdministratorDTO;
 import com.backend.easyturn.repositories.AdministratorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +15,62 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
 
-    public int createAdministrator(Administrator administrator){
-        Administrator adminCreated = administratorRepository.save(administrator);
-        return adminCreated.getIdAdministrator();
+    public AdministratorDTO createAdministrator(Administrator administrator){
+        try{
+            Administrator adminCreated = administratorRepository.save(administrator);
+            AdministratorDTO adminDTO = new AdministratorDTO(adminCreated.getId(), adminCreated.getMail(), adminCreated.getName(), adminCreated.getLastName());
+            return adminDTO;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
     public void deleteAdministrator(int idAdministrator) {
-        this.administratorRepository.deleteById(idAdministrator);
+        try {
+            this.administratorRepository.deleteById(idAdministrator);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    public Administrator getAdministrator(int idAdministrator) {
-        return this.administratorRepository.findById(idAdministrator).get();
+
+    public AdministratorDTO getAdministrator(int idAdministrator) {
+        try{
+            Administrator administrator = this.administratorRepository.findById(idAdministrator).get();
+            AdministratorDTO administratorDTO = new AdministratorDTO(administrator.getId(), administrator.getMail(), administrator.getName(), administrator.getLastName());
+            return administratorDTO;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    public List<Administrator> getAllAdministrator() {
-        return this.administratorRepository.findAll();
+
+    public List<AdministratorDTO> getAllAdministrator() {
+        try{
+            List<AdministratorDTO> administratorDTOS=null;
+            List<Administrator> administrators = this.administratorRepository.findAll();
+            if(administrators.isEmpty()){
+                return null;
+            }
+            for(Administrator admin:administrators){
+                AdministratorDTO adminDTO = new AdministratorDTO(admin.getId(), admin.getMail(), admin.getName(), admin.getLastName());
+                administratorDTOS.add(adminDTO);
+            }
+            return administratorDTOS;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    public Administrator updateAdministrator (Administrator administrator) {
-        return this.administratorRepository.save(administrator);
-    } //creo que reemplaza el existente por id --> ver porque no estoy seguro (otra opcion es traerlo por id, setear los datos nuevos y volverlo a guardar)
+
+    public AdministratorDTO updateAdministrator (Administrator administrator) {
+        try{
+            Administrator admin = this.administratorRepository.findById(administrator.getId()).get();
+            admin.setMail(administrator.getMail());
+            admin.setPassword(administrator.getPassword());
+            Administrator administratorModified = this.administratorRepository.save(admin);
+            AdministratorDTO administratorDTO = new AdministratorDTO(administratorModified.getId(), administratorModified.getMail(), administratorModified.getName(), administratorModified.getLastName());
+            return administratorDTO;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
