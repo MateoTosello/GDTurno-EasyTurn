c9241873e35065e11feee0a71f692dd62c45ad25
Tiago Pieroni <tepieroni@gmail.com>
2024-09-13 16:56:17 -0300

    CRUD Professional

--- easyturn/src/main/java/com/backend/easyturn/repositories/HealthInsuranceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.easyturn.repositories;
-
-import com.backend.easyturn.entities.HealthInsurance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthInsuranceRepository extends JpaRepository<HealthInsurance, Integer> {
-}
